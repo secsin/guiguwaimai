d8328b9dada4ba9471f52a1cb9608979dcc5012e
secsin <32921301+secsin@users.noreply.github.com>
2022-08-02 18:23:21 +0800

    complete static pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Msite from "../pages/Msite/Msite.vue";
+import Search from "../pages/Search/Search.vue";
+import Order from "../pages/Order/Order.vue";
+import Profile from "../pages/Profile/Profile.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/msite",
+      name: "Msite",
+      component: Msite
+    },
+    {
+      path: "/search",
+      name: "Search",
+      component: Search
+    },
+    {
+      path: "/order",
+      name: "Order",
+      component: Order
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      component: Profile
+    },
+    {
+      path: "/",
+      redirect: "/msite"
     }
   ]
-})
+});
