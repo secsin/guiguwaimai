4f3e2d936e2ce24a8fbd1acf8ad2b74964ddeb2e
secsin <32921301+secsin@users.noreply.github.com>
2022-08-09 13:45:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {
       "/api": {
         // 匹配所有以 '/api'开头的请求路径
-        target: "localhost:4000", // 代理目标的基础路径,用localhost会报错我也不知道为啥
+        target: "http://localhost:4000", // 代理目标的基础路径,用localhost会报错我也不知道为啥
         changeOrigin: true, // 支持跨域
         pathRewrite: {
           // 重写路径: 去掉路径中开头的'/api'
